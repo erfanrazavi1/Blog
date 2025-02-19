accc4f767c7c38a5c69650f5c0b74c40b38e537e
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2025-02-19 19:39:52 +0330

    create blog model

--- core/core/settings.py
+++ core/core/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'accounts',
-    # 'blog',
-    # 'comments',
+    'accounts',
+    'blog',
+    'comments',
 ]
 
 MIDDLEWARE = [
