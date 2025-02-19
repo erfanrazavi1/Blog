036fce88296fe9a60f4d62cf0bbf293f8a86b9ed
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2025-02-19 19:08:11 +0330

    delete apps and install docker and docker compose

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t55m+v%rudowo(tp$q8pa9j5e!146e+xx_jn14xda$%gm4ctgi'
+SECRET_KEY = config('SECRET_KEY', default='test_secret')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="*",
+    cast=lambda v: [s.strip() for s in v.split(",")],
+)
 
 # Application definition
 
@@ -37,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
-    'blog',
-    'comments',
+    # 'accounts',
+    # 'blog',
+    # 'comments',
 ]
 
 MIDDLEWARE = [
